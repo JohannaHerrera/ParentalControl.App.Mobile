56147a8989fa349c73c39dcd350792eff34472f3
Johanna Herrera <johanna.herrera1a@gmail.com>
2021-12-20 21:50:18 -0500

    Navigation Menu

--- ParentalControl.App.Mobile/ParentalControl.App.Mobile/Views/HomePage.xaml.cs
+++ ParentalControl.App.Mobile/ParentalControl.App.Mobile/Views/HomePage.xaml.cs
@@ -16,5 +16,40 @@ namespace ParentalControl.App.Mobile.Views
         {
             InitializeComponent();
         }
+
+        private void Home_Clicked(object sender, EventArgs a)
+        {
+            Navigation.PushAsync(new HomePage());
+        }
+
+        private void InfantAccounts_Clicked(object sender, EventArgs a)
+        {
+
+        }
+
+        private void Device_Clicked(object sender, EventArgs a)
+        {
+            Navigation.PushAsync(new DevicePage());
+        }
+
+        private void Schedules_Clicked(object sender, EventArgs a)
+        {
+
+        }
+
+        private void Notifications_Clicked(object sender, EventArgs a)
+        {
+
+        }
+
+        private void MyAccount_Clicked(object sender, EventArgs a)
+        {
+
+        }
+
+        private void Logout_Clicked(object sender, EventArgs a)
+        {
+
+        }
     }
 }
