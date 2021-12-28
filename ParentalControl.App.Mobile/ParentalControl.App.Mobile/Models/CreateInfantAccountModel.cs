4feb833a3de7940795e6fa610615a43193396e58
(Estudiante) Nelson Hernandez Avila <nelsoningsoftware@gmail.com>
2021-12-28 10:37:07 -0500

    App InfantAccount

---
+++ ParentalControl.App.Mobile/ParentalControl.App.Mobile/Models/CreateInfantAccountModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParentalControl.App.Mobile.Models
+{
+    public class CreateInfantAccountModel
+    {
+        public string InfantName { get; set; }
+        public string InfantGender { get; set; }
+        public int ParentId { get; set; }
+    }
+}
---
+++ ParentalControl.App.Mobile/ParentalControl.App.Mobile/Views/CreateInfantAccountPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using ParentalControl.App.Mobile.Models;
+using ParentalControl.App.Mobile.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ParentalControl.App.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateInfantAccountPage : ContentPage
+    {        
+        public CreateInfantAccountPage()
+        {
+            InitializeComponent();
+            LoadInformation();
+        }
+
+        public void LoadInformation()
+        {
+            List<string> genderList = new List<string>()
+            {
+                "Masculino",
+                "Femenino"
+            };
+
+            //Cargo los géneros en el picker 
+            foreach (var gender in genderList)
+            {
+               infantGender.Items.Add(gender);
+            }
+        }
+
+        private async void CreateInfantAccount_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(infantName.Text))
+            {
+                _ = DisplayAlert("Error", "El nombre del infante no es válido.", "OK");
+            }
+            else if (infantGender.SelectedIndex == -1)
+            {
+                _ = DisplayAlert("Error", "Por Favor seleccione el género del infante.", "OK");
+            }
+            else
+            {
+                CreateInfantAccountModel createInfantAccountModel = new CreateInfantAccountModel();
+                createInfantAccountModel.ParentId = Convert.ToInt32(Preferences.Get("ParentId", "0"));
+                createInfantAccountModel.InfantName = infantName.Text;
+                createInfantAccountModel.InfantGender = infantGender.SelectedItem.ToString();
+
+                var response = await new InfantAccountService().CreateInfantAccount(createInfantAccountModel);
+
+                if (response.IsSuccess)
+                {
+                    _ = DisplayAlert("Aviso", "La información se actualizó correctamente.", "OK");
+                    _ = Navigation.PushAsync(new InfantAccountPage());
+                }
+                else
+                {
+                    _ = DisplayAlert("Error", "Ocurrió un error inesperado. Inténtelo de nuevo.", "OK");
+                    _ = Navigation.PushAsync(new HomePage());
+                }
+            }
+
+        }
+
+        private void Home_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HomePage());
+        }
+
+        private void InfantAccounts_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new InfantAccountPage());
+        }
+
+        private void Device_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DevicePage());
+        }
+
+        private void Schedules_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Notifications_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MyAccount_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Logout_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InfantGender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int position = infantGender.SelectedIndex;
+
+            if (position > -1)
+            {
+                infantGender.SelectedIndex = position;
+            }
+        }
+    }
+}
