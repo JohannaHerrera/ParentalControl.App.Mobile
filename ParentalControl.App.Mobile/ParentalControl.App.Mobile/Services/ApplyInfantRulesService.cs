19319dc0b85efbf5c9886a129f4b7af7cec3a651
Johanna Herrera <johanna.herrera1a@gmail.com>
2022-01-17 17:32:47 -0500

    Se finaliza el bloqueo de aplicaciones.

--- ParentalControl.App.Mobile/ParentalControl.App.Mobile/Services/ApplyInfantRulesService.cs
+++ ParentalControl.App.Mobile/ParentalControl.App.Mobile/Services/ApplyInfantRulesService.cs
@@ -7,6 +7,7 @@ using Android.Widget;
 using Java.IO;
 using Java.Util;
 using ParentalControl.App.Mobile.Views;
+using Plugin.DeviceInfo;
 using Plugin.LocalNotification;
 using System;
 using System.Collections;
@@ -39,7 +40,7 @@ namespace ParentalControl.App.Mobile.Services
                     {
                         await Task.Delay(5000);
 
-                        await BlockApps();
+                        BlockApps();
 
                     }
                 }
@@ -52,14 +53,16 @@ namespace ParentalControl.App.Mobile.Services
             });
         }
 
-        public async Task BlockApps()
+        public void BlockApps()
         {
-            await Task.Run(() =>
+            Task.Run(async () =>
             {
                 try
-                {
-                    var apps = Android.App.Application.Context.PackageManager.GetInstalledApplications(PackageInfoFlags.MatchAll);
+                {                 
                     ActivityManager amg = (ActivityManager)Android.App.Application.Context.GetSystemService(Context.ActivityService);
+
+                    #region Código de Prueba
+                    /*var apps = Android.App.Application.Context.PackageManager.GetInstalledApplications(PackageInfoFlags.MatchAll);
                     UsageStatsManager m = (UsageStatsManager)Android.App.Application.Context.GetSystemService(Context.UsageStatsService);
 
                     Android.App.Usage.UsageStatsInterval usageStatsInterval = Android.App.Usage.UsageStatsInterval.Daily;
@@ -67,7 +70,6 @@ namespace ParentalControl.App.Mobile.Services
 
                     string nombre = string.Empty;
                     string pkgName = string.Empty;
-
                     foreach (var app in apps)
                     {
                         string name = app.LoadLabel(Android.App.Application.Context.PackageManager);
@@ -102,34 +104,63 @@ namespace ParentalControl.App.Mobile.Services
                             NotificationCenter.Current.Show(notification);
 
                         }                           
-                    }
+                    }*/
+                    #endregion Código de Prueba
 
-                    foreach (var app in amg.RunningAppProcesses)
+                    string deviceCode = CrossDeviceInfo.Current.Id;
+                    var response = await new GetDeviceConfigurationService().GetDeviceConfiguration(deviceCode);
+                    
+                    if(response != null)
                     {
-                        if (app.ProcessName.ToLower().Contains("brow"))
+                        if(response.AppsLocked != null)
                         {
-                            Intent startMain = new Intent(Intent.ActionMain);
-                            startMain.AddCategory(Intent.CategoryHome);
-                            startMain.SetFlags(ActivityFlags.NewTask);
-                            Android.App.Application.Context.StartActivity(startMain);
-
-                            Android.OS.Process.SendSignal(app.Pid, Android.OS.Signal.Kill);
-                            amg.KillBackgroundProcesses(app.ProcessName);
-                            amg.KillBackgroundProcesses(app.PkgList[0]);
-                            Android.OS.Process.KillProcess(app.Pid);
-
-                            var notification = new NotificationRequest
+                            if(response.AppsLocked.Count > 0)
                             {
-                                NotificationId = 100,
-                                Title = "¡Atención!",
-                                Description = $"No puedes usar la aplicación {app.ProcessName}",
-                                ReturningData = "Dummy data", // Returning data when tapped on notification.
-                            };
+                                var apps = Android.App.Application.Context.PackageManager.GetInstalledApplications(PackageInfoFlags.MatchAll);
 
-                            NotificationCenter.Current.Show(notification);
+                                foreach (var appLock in response.AppsLocked)
+                                {
+                                    string processName = string.Empty;
+
+                                    foreach (var appItem in apps)
+                                    {
+                                        string name = appItem.LoadLabel(Android.App.Application.Context.PackageManager);
+
+                                        if (name.ToLower().Contains(appLock.AppName.ToLower()))
+                                        {
+                                            processName = appItem.ProcessName;
+                                            break;
+                                        }
+                                    }
+
+                                    foreach (var app in amg.RunningAppProcesses)
+                                    {
+                                        if (app.ProcessName.ToLower().Contains(processName.ToLower()))
+                                        {
+                                            Intent startMain = new Intent(Intent.ActionMain);
+                                            startMain.AddCategory(Intent.CategoryHome);
+                                            startMain.SetFlags(ActivityFlags.NewTask);
+                                            Android.App.Application.Context.StartActivity(startMain);
+
+                                            Android.OS.Process.SendSignal(app.Pid, Android.OS.Signal.Kill);
+                                            amg.KillBackgroundProcesses(app.PkgList[0]);
+                                            Android.OS.Process.KillProcess(app.Pid);
+
+                                            var notification = new NotificationRequest
+                                            {
+                                                NotificationId = 100,
+                                                Title = "¡Atención!",
+                                                Description = $"No puedes usar la aplicación {appLock.AppName}",
+                                                ReturningData = "Dummy data", // Returning data when tapped on notification.
+                                            };
+
+                                            await NotificationCenter.Current.Show(notification);
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
-                    
                 }
                 catch(Exception ex)
                 {
